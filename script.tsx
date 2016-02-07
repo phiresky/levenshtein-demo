5b6ae735fa725d1a5342c973c2993a3efa400a6b
phiresky <phireskyde+git@gmail.com>
2016-02-07 20:12:42 +0100

    highlighting, synchronization, presets

--- script.tsx
+++ script.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-
+type Levenpath = {i:number, j:number, type?:string}[];
+const presets = "elephant|relevant,Saturday|Sunday,Google|Facebook".split(",").map(x => x.split("|"));
 function levenshtein(str1: string, str2: string) {
 	let m: number[][] = [], paths: [number, number][][] = [],
 		l1 = str1.length, l2 = str2.length;
@@ -30,7 +31,7 @@ function levenshtein(str1: string, str2: string) {
 		}
 
 	// one of many possible paths
-	let levenpath: {i:number, j:number, type?:string}[] = [];
+	let levenpath: Levenpath = [];
 	let j = l2;
 	for (let i = l1; i >= 0 && j >= 0;)
 		for (j = l2; i >= 0 && j >= 0;) {
@@ -40,6 +41,14 @@ function levenshtein(str1: string, str2: string) {
 			j = paths[t][j][1];
 		}
 	levenpath = levenpath.reverse();
+	for (let i = 0; i < levenpath.length; i++) {
+		const last = levenpath[i - 1], cur = levenpath[i];
+		if (i !== 0) {
+			if (cur.i === last.i + 1 && cur.j === last.j + 1 && m[cur.i][cur.j] !== m[last.i][last.j]) cur.type = 'replace';
+			else if (cur.i === last.i && cur.j === last.j + 1) cur.type = 'insert';
+			else if (cur.i === last.i + 1 && cur.j === last.j) cur.type = 'delete';
+		}
+	}
 	return { matrix: m, levenpath: levenpath };
 }
 
@@ -47,82 +56,120 @@ function loop<T>(i: number, callback: (i:number) => T): T[] {
 	return Array.apply(null, new Array(i)).map((e:any, i:number) => callback(i));
 }
 
-class Gui extends React.Component<{}, {str1: string, str2: string}> {
+const initialState = {str1:"", str2:"", highlight: {i:-1, j:-1}, showTrivial: true};
+type GuiState = typeof initialState;
+class Gui extends React.Component<{}, GuiState> {
+	matrix: number[][];
+	levenpath: Levenpath;
 	constructor() {
 		super();
-		this.state = {str1:"", str2:""};
+		this.state = initialState;
 	}
-	onClick(e: Event) {
+	onClick() {
 		this.setState({str1: $('#str1').val().trim().toLowerCase(),
 			str2: $('#str2').val().trim().toLowerCase()});
 	}
 	render() {
-		const {str1, str2} = this.state;
-		const {matrix,levenpath} = levenshtein(str1, str2);
-		for (let i = 0; i < levenpath.length; i++) {
-			const last = levenpath[i - 1], cur = levenpath[i];
-			if (i !== 0) {
-				if (cur.i === last.i + 1 && cur.j === last.j + 1 && matrix[cur.i][cur.j] !== matrix[last.i][last.j]) cur.type = 'replace';
-				else if (cur.i === last.i && cur.j === last.j + 1) cur.type = 'insert';
-				else if (cur.i === last.i + 1 && cur.j === last.j) cur.type = 'delete';
-			}
-		}
-		const t = levenpath.filter(x => !!x.type);
-		const steps = t.map((cur,i) => {
-			const dot = i < t.length - 1 ? ",":".";
-			switch(cur.type) {
-				case 'replace': return <p>replace <em>{str1.charAt(cur.i - 1)}</em> with <em>{str2.charAt(cur.j - 1)}</em> at position {cur.i}{dot}</p>;
-				case 'insert': return <p>insert <em>{str2.charAt(cur.j - 1)}</em> at position {cur.i}{dot}</p>;
-				case 'delete': return <p>delete <em>{str1.charAt(cur.i - 1)}</em> at position {cur.i}{dot}</p>;
-			}
-			return '';
-		});
-		steps.unshift(<p>The Levenshtein distance is <strong>{matrix[str1.length][str2.length]}</strong>:</p>);
-		return (
-			<div>
-				<div className="page-header">
-					<h1>Levenshtein Distance Calculator</h1>
-					<p>How many insertions, deletions, and substitutions does it take to turn <input id="str1" className="str" defaultValue="elephant" placeholder="word 1" tabIndex={1} /> into <input id="str2" className="str" defaultValue="relevant" placeholder="word 1" tabIndex={2} />?</p>
-					<button type="button" className="btn btn-primary" onClick={this.onClick.bind(this)} tabIndex={3}>Calculate Levenshtein distance</button>
-				</div>
-				{str1.length > 0 && str2.length > 0 ?
+		const {str1, str2, highlight} = this.state;
+		const header = 
+			<div className="page-header">
+				<h1>Levenshtein Distance Calculator</h1>
+				<p>How many insertions, deletions, and substitutions does it take to turn <input id="str1" className="str" defaultValue={str1} placeholder="word 1" tabIndex={1} /> into <input id="str2" className="str" defaultValue={str2} placeholder="word 2" tabIndex={2} />?</p>
+				<p>Try {presets.map((p,i) => 
+					<a key={i} onClick={() => this.setState({str1:p[0], str2:p[1]})}><i>{p[0]}</i> and <i>{p[1]}</i>{i==presets.length-1?".":", "}</a>
+				)}</p>
+				<button type="button" className="btn btn-primary" onClick={this.onClick.bind(this)} tabIndex={3}>Calculate Levenshtein distance</button>
+			</div>;
+		const matrix = this.matrix, levenpath = this.levenpath;
+		
+		if(matrix) {
+			let l = levenpath;
+			if(this.state.showTrivial) l = l.slice(1);
+			else l = l.filter(x => !!x.type);
+			const steps = l.map((cur,i) => {
+				let className = highlight.i == cur.i && highlight.j == cur.j ? "highlighted":"";
+				const fragment = (() => {
+					switch(cur.type) {
+						case 'replace': return <span><em>{str1.charAt(cur.i - 1)}</em> with <em>{str2.charAt(cur.j - 1)}</em></span>;
+						case 'insert': return <em>{str2.charAt(cur.j - 1)}</em>;
+						case 'delete': return <em>{str1.charAt(cur.i - 1)}</em>;
+						default:
+							className += " grayed"; 
+							return <span className="grayed">don't change <em>{str1.charAt(cur.i-1)}</em></span>
+					}
+				})();
+				return <li className={className}
+					onMouseEnter={() => this.setState({highlight:{i:cur.i,j:cur.j}})}
+					onMouseLeave={() => this.setState({highlight:{i:-1,j:-1}})} key={cur.i+","+cur.j}
+				><i>{str2.substr(0, cur.j-1)}<b>{str2[cur.j - 1]}</b>{str1.substr(cur.i)}</i>: {cur.type} {fragment} at position {cur.i}</li>;
+			});
+			return (
+				<div>
+					{header}
 					<div id="result">
 						<table ref="table">
 							<thead><tr><th></th><th></th>{str2.split('').map((c,i) => <th key={i}>{c}</th>)}</tr></thead>
 							<tbody>{loop(str1.length+1, i => 
-								<tr>
+								<tr key={i}>
 									<th>{i>0?str1.charAt(i - 1):""}</th>
-									{loop(str2.length+1, j => <td key={j} style={{opacity:0, fontWeight:levenpath.some(p => p.i==i&&p.j==j)?'700':'inherit'}}>{matrix[i][j]}</td>)}
+									
+									{loop(str2.length+1, j => <td key={j}
+										style={{opacity:0, fontWeight:levenpath.some(p => p.i==i&&p.j==j)?'700':'inherit'}}
+										className={highlight.i==i&&highlight.j==j?"highlighted":''}
+										onMouseEnter={evt => this.setState({highlight: {i,j}})}
+										onMouseLeave={evt => this.setState({highlight: {i:-1,j:-1}})}
+										>{matrix[i][j]}</td>)}
 								</tr>)}
 							</tbody>
 						</table>
-						{steps}
+						<div ref="resultText">
+						<p>The Levenshtein distance is <strong>{matrix[str1.length][str2.length]}</strong>:</p>
+						<ul>{steps}</ul>
+						<label><input type="checkbox" checked={this.state.showTrivial} onChange={e => this.setState({showTrivial:(e.target as HTMLInputElement).checked})} /> Show non-changes</label>
+						</div>
 					</div>
-				: <p>Don't know what to do? Try <i>elephant</i> and <i>relevant</i>, <i>Saturday</i> and <i>Sunday</i>, or <i>Google</i> and <i>Facebook</i>.</p>}
-      		</div>
-    	);
+				</div>
+			);
+		} else {
+			return (
+				<div>
+					{header}
+				</div>
+			);
+		}
 	}
 	componentDidMount() {
 		const keyupspan = $('<span style="width:auto;visibility:hidden;"></span>');
 		$('body').append(keyupspan);
 		$('input.str')
-			.on('keyup', function() {
+			.on('change', function() {
 				keyupspan.text($(this).val());
 				$(this).stop(true).animate({ 'min-width': 8 + ~~keyupspan.width() }, 200);
 			})
-			.on('keypress', function(e) {
-				if (e.which === 13)
-					$('#submit').click();
+			.on('keypress', e => {
+				if (e.which === 13) this.onClick();
 			});
 	}
-	componentDidUpdate() {
-		for (let i = 0; i <= this.state.str1.length; i++)
-			for (let j = 0; j <= this.state.str2.length; j++) {
-				$(this.refs["table"]).find('tr').eq(i + 1).find('td').eq(j).css({opacity:0}).delay(Math.max(i, j) * 200).animate({ opacity: 1 }, 400);
-			}
-					// after all cells fade in
-		$('#result').find('p').css('opacity', 0).delay(Math.max(this.state.str1.length, this.state.str2.length) * 200).animate({ opacity: 1 }, 400);
+	getTableCell(i: number, j: number): HTMLTableCellElement {
+		return (this.refs["table"] as any).tBodies[0].children[i].children[j+1];
+	}
+	componentWillUpdate(nextProps: any, nextState: GuiState) {
+		if(nextState.str1 !== this.state.str1 || nextState.str2 !== this.state.str2) {
+			const ret = levenshtein(nextState.str1, nextState.str2);
+			this.levenpath = ret.levenpath; this.matrix = ret.matrix;
+			$("#str1").val(nextState.str1).change(); $("#str2").val(nextState.str2).change();
+		}
+	}
+	componentDidUpdate(prevProps: any, prevState: GuiState) {
+		if(prevState.str1 !== this.state.str1 || prevState.str2 !== this.state.str2) {
+			for (let i = 0; i <= this.state.str1.length; i++)
+				for (let j = 0; j <= this.state.str2.length; j++) {
+					$(this.getTableCell(i,j)).css({opacity:0}).delay(Math.max(i, j) * 200).animate({ opacity: 1 }, 400);
+				}
+						// after all cells fade in
+			$(this.refs["resultText"]).css('opacity', 0).delay(Math.max(this.state.str1.length, this.state.str2.length) * 200).animate({ opacity: 1 }, 400);
+		}
 	}
 }
 
-ReactDOM.render(<Gui/>, document.getElementById("reactContent"));
+(window as any).gui = ReactDOM.render(<Gui/>, document.getElementById("reactContent"));
